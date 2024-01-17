a206e8dd93c600ea46ee06d71e83f456f0567305
fennefitskie <99078521@mydavinci.nl>
2024-01-17 15:26:25 +0100

    Fout in de constructie

--- dungeon.py
+++ dungeon.py
@@ -35,30 +35,32 @@ print('Je zie een deur achter het standbeeld.')
 print('')
 time.sleep(1)
 
-# === [kamer 6] === #
-zombie_attack = 1
-zombie_defense = 0
-zombie_health = 2
-print(f'Dapper met je handen loop je de kamer binnen.')
-print('Je loopt tegen een zombie aan.')
+dekamerkeuze = input("Wil jij naar kamer 3 of naar kamer 6? ")
 
-zombie_hit_damage = (zombie_attack - player_defense)
-if zombie_hit_damage <= 0:
-    print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
-else:
-    zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
-    
-    player_hit_damage = (player_attack - zombie_defense)
-    player_attack_amount = math.ceil(zombie_health / player_hit_damage)
+if dekamerkeuze == 'kamer 6':
+    zombie_attack = 1
+    zombie_defense = 0
+    zombie_health = 2
+    print(f'Dapper met je handen loop je de kamer binnen.')
+    print('Je loopt tegen een zombie aan.')
 
-    if player_attack_amount < zombie_attack_amount:
-        print(f'In {player_attack_amount} rondes versla je de zombie.')
-        player_health -= zombie_attack_amount * zombie_hit_damage
-        print(f'Je health is nu {player_health}.')
+    zombie_hit_damage = (zombie_attack - player_defense)
+    if zombie_hit_damage <= 0:
+        print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
     else:
-        print('Helaas is de zombie te sterk voor je.')
-        print('Game over.')
-        exit()
+        zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
+    
+        player_hit_damage = (player_attack - zombie_defense)
+        player_attack_amount = math.ceil(zombie_health / player_hit_damage)
+
+        if player_attack_amount < zombie_attack_amount:
+            print(f'In {player_attack_amount} rondes versla je de zombie.')
+            player_health -= zombie_attack_amount * zombie_hit_damage
+            print(f'Je health is nu {player_health}.')
+        else:
+            print('Helaas is de zombie te sterk voor je.')
+            print('Game over.')
+            exit()
 
 
 
@@ -70,23 +72,26 @@ if willekeurig_itemlijst == "zwaard":
 elif willekeurig_itemlijst == "schild":
     player_defense += 1
 
-print('Je duwt hem open en stap een hele lange kamer binnen.')
-print(f'In deze kamer staat een tafel met daarop een {willekeurig_itemlijst}.')
-print(f'Je pakt het {willekeurig_itemlijst} op en houd het bij je.')
-print('Op naar de volgende deur.')
-print('')
-time.sleep(1)
+    
+elif dekamerkeuze == 'kamer 3':
+
+    print('Je duwt hem open en stap een hele lange kamer binnen.')
+    print(f'In deze kamer staat een tafel met daarop een {willekeurig_itemlijst}.')
+    print(f'Je pakt het {willekeurig_itemlijst} op en houd het bij je.')
+    print('Op naar de volgende deur.')
+    print('')
+    time.sleep(1)
 
 # === [kamer 4] === #
 vijand_attack = 2
 vijand_defense = 0
 vijand_health = 3
 print(f'Dapper met je nieuwe {willekeurig_itemlijst} loop je de kamer binnen.')
-print('Je loopt tegen een zombie aan.')
+print('Je loopt tegen de vijand aan.')
 
 vijand_hit_damage = (vijand_attack - player_defense)
 if vijand_hit_damage <= 0:
-    print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
+    print('Jij hebt een te goede verdediging voor de vijand, hij kan je geen schade doen.')
 else:
     vijand_attack_amount = math.ceil(player_health / vijand_hit_damage)
     
@@ -94,11 +99,11 @@ else:
     player_attack_amount = math.ceil(vijand_health / player_hit_damage)
 
     if player_attack_amount < vijand_attack_amount:
-        print(f'In {player_attack_amount} rondes versla je de zombie.')
+        print(f'In {player_attack_amount} rondes versla je de vijand.')
         player_health -= vijand_attack_amount * vijand_hit_damage
         print(f'Je health is nu {player_health}.')
     else:
-        print('Helaas is de zombie te sterk voor je.')
+        print('Helaas is de vijand te sterk voor je.')
         print('Game over.')
         exit()
 print('')
